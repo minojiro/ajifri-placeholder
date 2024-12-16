c836ea0882cac06ed4a639fbf7560b267aa90870
minojiro <minojiro@users.noreply.github.com>
2024-12-16 20:44:44 +0900

    chore: Added type declarations

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 const arr = ["アジフライ", "ドボ", "お刺身", "食え", "むむ…", "あ"];
 
-export const ajifriPlaceholder = (length?: number, lengthMax?: number) => {
-  if ((length ?? 1) <= -1) return "";
+export const ajifriPlaceholder = (
+  length?: number,
+  lengthMax?: number
+): string => {
+  if ((length ?? 50) <= -1) return "";
   if ((lengthMax ?? 1) <= -1) return "";
 
   length = length ?? 1;
@@ -15,7 +18,7 @@ export const ajifriPlaceholder = (length?: number, lengthMax?: number) => {
 
   do {
     t += arr[Math.floor(Math.random() * arr.length)].repeat(
-      Math.floor(Math.random() * 5) + 1
+      Math.floor(Math.random() * 3) + 1
     );
   } while (t.length < resultLength);
 
